c6749303a6105cd37fa5dff9a2b173ea615c125a
Caio <caioccastro1@gmail.com>
2024-03-25 20:28:02 -0300

    v2

--- App.tsx
+++ App.tsx
@@ -10,11 +10,21 @@ import {
 
 import { RadioButton } from "react-native-paper";
 
+import { useFonts } from "@expo-google-fonts/roboto";
+
 import { Roboto_900Black } from "@expo-google-fonts/roboto";
 
 const App = () => {
   const [checkedJuros, setCheckedJuros] = React.useState("");
   const [checkedTempo, setCheckedTempo] = React.useState("");
+
+  const [fontsLoaded] = useFonts({
+    Roboto_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -22,94 +32,101 @@ const App = () => {
         <Text style={styles.title}>Calculadora de Juros Simples</Text>
       </View>
 
-      <View>
-        <Text>Capital</Text>
-        <TextInput style={styles.input} />
-      </View>
+      <View style={styles.container_form}>
+        <View>
+          <Text>Capital</Text>
+          <TextInput style={styles.input} />
+        </View>
 
-      <View>
-        <Text>Juros</Text>
-        <TextInput style={styles.input} />
-
-        <View style={styles.radios}>
-          <View style={styles.radios}>
-            <RadioButton
-              value="mes"
-              status={checkedJuros === "mes" ? "checked" : "unchecked"}
-              onPress={() => {
-                setCheckedJuros("mes");
-              }}
-            />
-            <Text>Mês</Text>
-          </View>
+        <View>
+          <Text>Juros</Text>
+          <TextInput style={styles.input} />
 
           <View style={styles.radios}>
-            <RadioButton
-              value="dia"
-              status={checkedJuros === "dia" ? "checked" : "unchecked"}
-              onPress={() => {
-                setCheckedJuros("dia");
-              }}
-            />
-            <Text>Dia</Text>
-          </View>
+            <View style={styles.radios}>
+              <RadioButton
+                value="mes"
+                status={checkedJuros === "mes" ? "checked" : "unchecked"}
+                onPress={() => {
+                  setCheckedJuros("mes");
+                }}
+              />
+              <Text>Mês</Text>
+            </View>
 
-          <View style={styles.radios}>
-            <RadioButton
-              value="ano"
-              status={checkedJuros === "ano" ? "checked" : "unchecked"}
-              onPress={() => {
-                setCheckedJuros("ano");
-              }}
-            />
-            <Text>Ano</Text>
+            <View style={styles.radios}>
+              <RadioButton
+                value="dia"
+                status={checkedJuros === "dia" ? "checked" : "unchecked"}
+                onPress={() => {
+                  setCheckedJuros("dia");
+                }}
+              />
+              <Text>Dia</Text>
+            </View>
+
+            <View style={styles.radios}>
+              <RadioButton
+                value="ano"
+                status={checkedJuros === "ano" ? "checked" : "unchecked"}
+                onPress={() => {
+                  setCheckedJuros("ano");
+                }}
+              />
+              <Text>Ano</Text>
+            </View>
           </View>
         </View>
-      </View>
 
-      <View>
-        <Text>Tempo</Text>
-        <TextInput style={styles.input} />
-
-        <View style={styles.radios}>
-        <View style={styles.radios}>
-            <RadioButton
-              value="mes"
-              status={checkedTempo === "mes" ? "checked" : "unchecked"}
-              onPress={() => {
-                setCheckedTempo("mes");
-              }}
-            />
-            <Text>Mês</Text>
-          </View>
+        <View>
+          <Text>Tempo</Text>
+          <TextInput style={styles.input} />
 
           <View style={styles.radios}>
-            <RadioButton
-              value="dia"
-              status={checkedTempo === "dia" ? "checked" : "unchecked"}
-              onPress={() => {
-                setCheckedTempo("dia");
-              }}
-            />
-            <Text>Dia</Text>
-          </View>
+            <View style={styles.radios}>
+              <RadioButton
+                value="mes"
+                status={checkedTempo === "mes" ? "checked" : "unchecked"}
+                onPress={() => {
+                  setCheckedTempo("mes");
+                }}
+              />
+              <Text>Mês</Text>
+            </View>
 
-          <View style={styles.radios}>
-            <RadioButton
-              value="ano"
-              status={checkedTempo === "ano" ? "checked" : "unchecked"}
-              onPress={() => {
-                setCheckedTempo("ano");
-              }}
-            />
-            <Text>Ano</Text>
+            <View style={styles.radios}>
+              <RadioButton
+                value="dia"
+                status={checkedTempo === "dia" ? "checked" : "unchecked"}
+                onPress={() => {
+                  setCheckedTempo("dia");
+                }}
+              />
+              <Text>Dia</Text>
+            </View>
+
+            <View style={styles.radios}>
+              <RadioButton
+                value="ano"
+                status={checkedTempo === "ano" ? "checked" : "unchecked"}
+                onPress={() => {
+                  setCheckedTempo("ano");
+                }}
+              />
+              <Text>Ano</Text>
+            </View>
           </View>
         </View>
-      </View>
 
-      <View>
-        <Text>Montante</Text>
-        <TextInput style={styles.input} />
+        <View>
+          <Text>Montante</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text_white}>Calcular</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -121,7 +138,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
+  },
+
+  container_form: {
+    flex: 0.6,
+    backgroundColor: "#adadad",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    borderRadius: 30,
+    padding: 20,
   },
 
   radios: {
@@ -129,11 +155,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flexDirection: "row",
   },
-  
-  title:{
-    paddingBottom:100,
+
+  title: {
+    paddingBottom: 100,
     fontSize: 30,
-    fontFamily: 'Roboto_900Black',
+    fontFamily: "Roboto_900Black",
   },
 
   input: {
@@ -141,6 +167,7 @@ const styles = StyleSheet.create({
     height: 40,
     borderWidth: 1,
     borderRadius: 10,
+    backgroundColor: "#fff",
   },
 
   input_group: {
@@ -154,7 +181,7 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    backgroundColor: "red",
+    backgroundColor: "blue",
     width: 200,
     height: 35,
     borderRadius: 10,
@@ -163,6 +190,10 @@ const styles = StyleSheet.create({
     marginRight: 30,
     marginTop: 15,
     paddingTop: 10,
+  },
+
+  text_white: {
+    color: "white",
   },
 });
 
